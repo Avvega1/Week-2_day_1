cbaf484e67f3bf9d49b685af0044db09197e5b23
Vega <avvega1@cps.k12.il.us>
2024-09-04 14:59:34 -0500

    first commit

---
+++ week2_python/week2.py
@@ -0,0 +1,12 @@
+# ask someone what day it is
+day = input("what day is it?")
+print("the day is"+ day)
+# ask them for their favorite holiday
+holiday = input("What is your favorite holiday")
+print("favorite holiday is"+ holiday)
+# print out the day and their favorite holiday
+# a sentence
+print ("the day is" + day +
+       "favorite holiday is"
+       + holiday)
+# making a change to the file
